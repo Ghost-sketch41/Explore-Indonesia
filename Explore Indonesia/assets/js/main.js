a4e6260ddf11a0f11406e46e03f9a7b435a1a68c
Ghost-sketch41 <nl818383@gmail.com>
2024-10-06 20:29:51 +0700

    Add files via upload

---
+++ Explore Indonesia/assets/js/main.js
@@ -0,0 +1,60 @@
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+      640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+
+      920: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    }
+  });
